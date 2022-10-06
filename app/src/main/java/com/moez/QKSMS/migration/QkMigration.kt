0f7c7713b479ad3ee44341900ec558738dc7dccc
Muntashir Al-Islam <muntashirakon@riseup.net>
2022-10-06 10:58:47 +0600

    Remove migrations
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/com/moez/QKSMS/migration/QkMigration.kt
+++ app/src/main/java/com/moez/QKSMS/migration/QkMigration.kt
@@ -38,31 +38,10 @@ class QkMigration @Inject constructor(
         GlobalScope.launch {
             val oldVersion = prefs.version.get()
 
-            if (oldVersion < 2199) {
-                upgradeTo370()
-            }
+            // TODO: Do something in case of an update
 
             prefs.version.set(context.versionCode)
         }
-    }
-
-    private fun upgradeTo370() {
-        // Migrate changelog version
-        prefs.changelogVersion.set(prefs.version.get())
-
-        // Migrate from old SIA preference to blocking manager preference
-        if (prefs.sia.get()) {
-            prefs.blockingManager.set(Preferences.BLOCKING_MANAGER_SIA)
-            prefs.sia.delete()
-        }
-
-        // Migrate blocked conversations into QK blocking client
-        val addresses = conversationRepo.getBlockedConversations()
-                .flatMap { conversation -> conversation.recipients }
-                .map { recipient -> recipient.address }
-                .distinct()
-
-        qksmsBlockingClient.block(addresses).blockingAwait()
     }
 
 }
